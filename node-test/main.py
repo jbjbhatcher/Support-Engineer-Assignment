5390d077746be42a8a190a47c0618a35160c3b6f
Josh Hatcher <jbhtcher@memphis.edu>
2018-02-20 22:59:39 -0600

    Added properties to the class - api_name. Also added a __str__ method.

--- node-test/main.py
+++ node-test/main.py
@@ -8,27 +8,40 @@ from selenium import webdriver
 
 username = 'jbhtcher@memphis.edu' # username to log in with
 authkey = 'u63c4853726aabbb' # auth token
+
 endpoint = "https://crossbrowsertesting.com/api/v3/selenium/browsers?format=json"
 all_browsers_available = json.loads(urllib2.urlopen(endpoint).read())
 
-# keep track of machine state w/ class
 class Machine():
-    def __init__(self, type):
+    def __init__(self, name):
+        self.asJson = None
+        for i in range(len(all_browsers_available)): # purpose of this loop is to iterate over list of all possible browsers and to find the first matching occurence of a machine
+            if all_browsers_available[i]['name'] == name and self.asJson is None:
+                self.asJson = all_browsers_available[i]
+                break # found the value so break
+        self.api_name = self.asJson['api_name']
 
-        self.type = type # name of the machine to be tested
-        # self.version =
-        # self.platform =
-        # self.screenResolution =
-
-        self.didPass = None
-
+    def __str__( self ):
+        return 'The machine name is: ' + self.asJson['name']
         # convert the json data to python data
 
     def test_local_title(self): # should set Machine.didPass to True on success
         pass
 
+def get_all():
+    out_array = []
+    for i in range(len(all_browsers_available)):
+        out_array.append (all_browsers_available[i]['name'])
+    return out_array
 
 # create machine instances
-print all_browsers_available[0]['name']
-print all_browsers_available[1]['name']
-print all_browsers_available[2]['name']
+windows_machine = Machine('Windows 10')
+mac_machine = Machine('Mac OSX 10.9')
+mobile_machine = Machine('Android Galaxy Note 3 / 4.4')
+
+# print get_all()
+
+# print machines json value (if these print, it means the machines have been successfully retrieved & you can interact with them in python)
+print windows_machine
+print mac_machine
+print mobile_machine
